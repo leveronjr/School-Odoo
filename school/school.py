c4635c1e5121212ec696cfaa033a0870ca78d512
Leveronjr <jkleveronjr@gmail.com>
2019-03-14 02:35:06 -0600

    correcciones

--- school/school.py
+++ school/school.py
@@ -4,9 +4,10 @@
 from odoo import models, fields
 
 class School (models.Model):
-	_name = 'school.schol'
+	_name = 'alumno.alumn'
 	nombreAlumno = fields.Char(string='nombre_completo',requiered=True)
 	edad = fields.Integer(string="edad",requiered=True)
 	sexo = fields.Char(string="sexo",requiered=True)
 	encargado = fields.Char(string="encargado")
 	fecha_matricula = fields.Date(string='fecha_matricula')
+	photo = field.Binary(string="Photo")
