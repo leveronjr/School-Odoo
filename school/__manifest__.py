f45f2390c2524fd266ad33302d8eb34ce9bbc1b2
Leveronjr <jkleveronjr@gmail.com>
2019-03-19 20:39:51 -0600

    New

--- school/__manifest__.py
+++ school/__manifest__.py
@@ -6,22 +6,19 @@
     'version': '12.0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'sale'],
+    'depends': ['base'],
 
     # always loaded
     'data': [
         #'data/hn_sale_data.xml',
-        #'data/hn_sale_sequence.xml',
-        'security/rules.xml',
-        'security/ir.model.access.csv',
-        
+        #'data/hn_sale_sequence.xml',        
         #'wizard/consolidate_orders.xml'
         #'report/report_layout.xml',
         #'report/account_invoice.xml',
         #'report/poa_report_templates.xml',
         #'report/poa_report_pivot.xml',
         #'report/pei_pivot.xml',
-        'views/school_view.xml',
+        'data/school_view.xml',
         
     ],
     # only loaded in demonstration mode
--- school/school.py
+++ school/school.py
@@ -1,24 +1,13 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import api, exceptions, fields, models, _
+from odoo import api, exceptions, fields, models
 
 class School(models.Model):
 
     _name = 'alumno.school'
-
-
     name = fields.Char(string='Nombre', required=True)
-    identidad = fields.Char(string='Identidad')
-
+    edad = fields.Integer(string='Edad')
     genero = fields.Selection(string='Género', 
         selection=[('m', 'Másculino'), ('f', 'Feménino'),])
-    
     date_birth = fields.Date(string='Fecha de Nacimiento')
-
-
-    
-class SaleOrder(models.Model):
-    _inherit = ['sale.order']
-
-   alumno_id = fields.Many2one(comodel_name='alumno.school', string='Alumno')
